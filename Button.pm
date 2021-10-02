6af7e198bb83ebc85bfe87ff1ed4138bba79ce43
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2021-10-02 12:22:00 +0200

    First version

---
+++ Button.pm
@@ -0,0 +1,242 @@
+package Tags::HTML::Login::Button;
+
+use strict;
+use warnings;
+
+use Class::Utils qw(set_params);
+use Error::Pure qw(err);
+
+our $VERSION = 0.01;
+
+# Constructor.
+sub new {
+	my ($class, @params) = @_;
+
+	# Create object.
+	my $self = bless {}, $class;
+
+	# 'CSS::Struct::Output' object.
+	$self->{'css'} = undef;
+
+	# Login button link.
+	$self->{'link'} = 'login';
+
+	# 'Tags::Output' object.
+	$self->{'tags'} = undef;
+
+	# Login button title.
+	$self->{'title'} = 'LOGIN';
+
+	# Process params.
+	set_params($self, @params);
+
+	# Check to 'CSS::Struct::Output' object.
+	if ($self->{'css'} && ! $self->{'css'}->isa('CSS::Struct::Output')) {
+		err "Parameter 'css' must be a 'CSS::Struct::Output::*' class.";
+	}
+
+	# Check to 'Tags' object.
+	if ($self->{'tags'} && ! $self->{'tags'}->isa('Tags::Output')) {
+		err "Parameter 'tags' must be a 'Tags::Output::*' class.";
+	}
+
+	# Object.
+	return $self;
+}
+
+# Process 'Tags'.
+sub process {
+	my ($self, $percent_value) = @_;
+
+	# Main content.
+	$self->{'tags'}->put(
+		['a', 'class', 'outer'],
+
+		['b', 'div'],
+		['a', 'class', 'login'],
+		['b', 'a'],
+		['a', 'href', $self->{'link'}],
+		['d', $self->{'title'}],
+		['e', 'a'],
+		['e', 'div'],
+	);
+
+	return;
+}
+
+# Process 'CSS::Struct'.
+sub process_css {
+	my $self = shift;
+
+	$self->{'css'}->put(
+		['s', '.outer'],
+		['d', 'position', 'fixed'],
+		['d', 'top', '50%'],
+		['d', 'left', '50%'],
+		['d', 'transform', 'translate(-50%, -50%)'],
+		['e'],
+
+		['s', '.login'],
+		['d', 'text-align', 'center'],
+		['d', 'background-color', 'blue'],
+		['d', 'padding', '1em'],
+		['e'],
+
+		['s', '.login a'],
+		['d', 'text-decoration', 'none'],
+		['d', 'color', 'white'],
+		['d', 'font-size', '3em'],
+		['e'],
+	);
+
+	return;
+}
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Tags::HTML::Login::Button - Tags helper for login button.
+
+=head1 SYNOPSIS
+
+ use Tags::HTML::Login::Button;
+
+ my $obj = Tags::HTML::Login::Button->new(%params);
+ $obj->process;
+ $obj->process_css;
+
+=head1 METHODS
+
+=head2 C<new>
+
+ my $obj = Tags::HTML::Login::Button->new(%params);
+
+Constructor.
+
+=over 8
+
+=item * C<css>
+
+'CSS::Struct::Output' object for L<process_css> processing.
+
+It's required.
+
+Default value is undef.
+
+=item * C<link>
+
+Login button link.
+
+Default value is 'login'.
+
+=item * C<tags>
+
+'Tags::Output' object.
+
+Default value is undef.
+
+=item * C<title>
+
+Login button title.
+
+Default value is 'LOGIN'.
+
+=back
+
+=head2 C<process>
+
+ $obj->process($percent_value);
+
+Process Tags structure for gradient.
+
+Returns undef.
+
+=head2 C<process_css>
+
+ $obj->process_css;
+
+Process CSS::Struct structure for output.
+
+Returns undef.
+
+=head1 ERRORS
+
+ new():
+         From Class::Utils::set_params():
+                 Unknown parameter '%s'.
+         Parameter 'css' must be a 'CSS::Struct::Output::*' class.
+         Parameter 'tags' must be a 'Tags::Output::*' class.
+
+=head1 EXAMPLE
+
+ use strict;
+ use warnings;
+
+ use CSS::Struct::Output::Indent;
+ use Tags::HTML::Login::Button;
+ use Tags::Output::Indent;
+
+ # Object.
+ my $css = CSS::Struct::Output::Indent->new;
+ my $tags = Tags::Output::Indent->new;
+ my $obj = Tags::HTML::Login::Button->new(
+         'css' => $css,
+         'tags' => $tags,
+ );
+
+ # Process indicator.
+ $obj->process_css;
+ $obj->process;
+
+ # Print out.
+ print "CSS\n";
+ print $css->flush."\n";
+ print "HTML\n";
+ print $tags->flush."\n";
+
+ # Output:
+ # TODO
+
+=head1 DEPENDENCIES
+
+L<Class::Utils>,
+L<Error::Pure>.
+
+=head1 SEE ALSO
+
+=over
+
+=item L<Tags::HTML::Stars>
+
+Tags helper for stars evaluation.
+
+=back
+
+=head1 REPOSITORY
+
+L<https://github.com/michal-josef-spacek/Tags-HTML-Login-Button>
+
+=head1 AUTHOR
+
+Michal Josef Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+© Michal Josef Špaček 2021
+
+BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
