4a09bd8a7008564f4c3abb9d6018278d7a713496
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-07-07 22:35:13 +0200

    Prepare for 0.02 version

--- Button.pm
+++ Button.pm
@@ -7,7 +7,7 @@ use warnings;
 use Class::Utils qw(set_params split_params);
 use Error::Pure qw(err);
 
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
 # Constructor.
 sub new {
@@ -251,6 +251,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.01
+0.02
 
 =cut
